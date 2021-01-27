07cecfb11740ca9cc1520503a0f19280a1a34b76
sigjak <sigjak@gmail.com>
2021-01-27 18:47:25 +0000

    before conn

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:weight_2/widgets/my_icons.dart';
-import '../screens/data_list_screen.dart';
+
 import '../screens/data_add_screen.dart';
-import '../screens/syst_diast_screen.dart';
+
 import '../screens/register_screen.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -31,22 +30,22 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context).pushReplacementNamed(Register.routeName);
             },
           ),
-          Divider(),
-          ListTile(
-            leading: Icon(MyIcons.chart_line),
-            title: Text('Show Data'),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(ListScreen.routeName);
-            },
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.favorite_border),
-            title: Text('BP Plots'),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(SystDiast.routeName);
-            },
-          ),
+          // Divider(),
+          // ListTile(
+          //   leading: Icon(MyIcons.chart_line),
+          //   title: Text('Show Data'),
+          //   onTap: () {
+          //     Navigator.of(context).pushReplacementNamed(ListScreen.routeName);
+          //   },
+          // ),
+          // Divider(),
+          // ListTile(
+          //   leading: Icon(Icons.favorite_border),
+          //   title: Text('BP Plots'),
+          //   onTap: () {
+          //     Navigator.of(context).pushReplacementNamed(SystDiast.routeName);
+          //   },
+          // ),
         ],
       ),
     );
